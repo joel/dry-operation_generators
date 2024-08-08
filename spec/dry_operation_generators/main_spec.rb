6cd0cedc59f537bff19f3e2ba4a11264b38c4a75
Joel AZEMAR <joel.azemar@gmail.com>
2024-08-08 16:36:03 +0200

    Add Main Spec

---
+++ spec/dry_operation_generators/main_spec.rb
@@ -0,0 +1,14 @@
+module DryOperationGenerators
+  RSpec.describe Main do
+    subject(:foo) { described_class.new }
+    describe '#dry_operation_generators' do
+      context 'when all is good' do
+        let(:input) { 'foo' }
+        let(:result) { 'foo' }
+        it do
+          expect(foo.dry_operation_generators(input)).to eql(result)
+        end
+      end
+    end
+  end
+end
